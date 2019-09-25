7772280f61ba0bfc348d5b2f847cc129c23a2d13
David-Rauch <52569940+David-Rauch@users.noreply.github.com>
2019-09-25 18:27:47 -0400

    Add files via upload
    I updated this file, and it works for the project I am creating.  If you are looking for a OFX converter to use for yourself it may need some editing.  Enjoy!

--- OFXConverter/Program.cs
+++ OFXConverter/Program.cs
@@ -7,7 +7,10 @@ namespace OFXConverter
         static void Main(string[] args)
         {
             string webSite = new string("c://Users/David/Desktop/Money/usaa_money.ofx");
-            FinancerData.ImportOfx.ToXElement(webSite);
+            OFXConverter.TryAgainConverter.DataPrinting(webSite);
+
+            // I created this "converter" to pull certain data fields from a .OFX file for my school project
+            // It will work for you as well, but you have to identify the tags in the file to flag and edit the TryAgainConverter class
         }
     }
 }
---
+++ OFXConverter/TryAgainConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+
+namespace OFXConverter
+{
+    public static class TryAgainConverter
+    {
+        
+        public static void DataPrinting(string pathToOfxFile)
+        {
+            if (!System.IO.File.Exists(pathToOfxFile))
+            {
+                throw new FileNotFoundException();
+            }
+            StreamReader read = null;
+            //string ofxFile = "";
+            FileStream fs = File.Open(pathToOfxFile, FileMode.Open, FileAccess.Read);
+            read = new StreamReader(fs, true);
+
+            bool inLoop = true;
+            string temp = read.ReadToEnd();
+            using (StreamWriter sr = new StreamWriter(@"C:\Users\David\Desktop\Money\OFXConvertTest.xml"))
+            {
+                sr.WriteLine("Type\tDate Posted\tTransaction Ammount\tName");
+                while (inLoop == true)
+                {
+                    int sorter = temp.IndexOf("<STMTTRN>");
+                    if (sorter > 0)
+                    {
+                        int a = temp.IndexOf("<TRNTYPE>") + 9;
+                        temp = temp.Substring(a).Trim();
+                        int pos_init1 = temp.IndexOf("<");
+                        string tranType = temp.Substring(0, pos_init1);
+
+                        int b = temp.IndexOf("<DTPOSTED>") + 10;
+                        temp = temp.Substring(b).Trim();
+                        int pos_init2 = temp.IndexOf("<");
+                        string dtPosted = temp.Substring(0, pos_init2 - 6);
+
+                        int c = temp.IndexOf("<TRNAMT>") + 8;
+                        temp = temp.Substring(c).Trim();
+                        int pos_init3 = temp.IndexOf("<");
+                        string trAmount = temp.Substring(0, pos_init3);
+
+                        int f = temp.IndexOf("<NAME>") + 6;
+                        temp = temp.Substring(f).Trim();
+                        int pos_init6 = temp.IndexOf("<");
+                        string name = temp.Substring(0, pos_init6);
+                        
+                        sr.WriteLine($"{tranType}\t{dtPosted}\t{trAmount}\t\t\t{name}\n");
+                    }
+                    else
+                    {
+                        sr.Flush();
+                        sr.Close();
+                        inLoop = false;
+                    }
+                }
+            }
+        }
+    }
+}
